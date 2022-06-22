b560924804de5adcf162cf6fbab8c07323604b90
twentiecker <twentiecker@gmail.com>
2022-06-22 08:56:06 +0700

    init state

--- src/components/MyForm.js
+++ src/components/MyForm.js
@@ -1,19 +1,29 @@
 import React from "react";
 
 class MyForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // inisialisasi state
+    this.state = {
+      name: "",
+      email: "",
+      gender: "Man",
+    };
+  }
   render() {
     return (
       <div>
         <h1>Register Form</h1>
         <form>
           <label for="name">Name :</label>
-          <input id="name" type="text" />
+          <input id="name" type="text" value={this.state.name} />
           <br />
           <label for="email">Email :</label>
-          <input id="email" type="text" />
+          <input id="email" type="text" value={this.state.email} />
           <br />
           <label for="gender">Gender :</label>
-          <select id="gender">
+          <select id="gender" value={this.state.gender}>
             <option value="Man">Man</option>
             <option value="Woman">Woman</option>
           </select>
